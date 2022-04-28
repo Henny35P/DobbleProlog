7bc5413ca99dae1303a3e2162256118988834393
Henny35P <hans.villarroel@usach.cl>
2022-04-28 01:39:55 -0400

    nCartas hecho

--- main.pl
+++ main.pl
@@ -7,14 +7,14 @@ nCartas_i(N,I,J,L):-
     I = 0,L = [1].
 nCartas_i(N,I,J,[Y | Xs]) :-
     I > 0,
-    Y is N * I + (J + 1),
+    Y is (N * J) + (I + 1),
     I1 is I - 1,
     nCartas_i(N,I1,J,Xs).
 
 nCartas(N,J,L):-
     J = 0, L = [1].
-nCartas(N,J,[Y | Xs]) :-
+nCartas(N,J,L) :-
     J>0,
     J1 is J - 1,
-    nCartas_i(N,N,J,Xs),
+    nCartas_i(N,N,J,L),
     nCartas(N,J1,Xs).
