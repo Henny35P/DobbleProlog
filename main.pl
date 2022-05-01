b19861fe73df38fafaffdf96de767dbbb1db89e7
Henny35P <hans.villarroel@usach.cl>
2022-04-30 22:37:39 -0400

    n2Cartas

--- main.pl
+++ main.pl
@@ -18,3 +18,28 @@ nCartas(N,J,L) :-
     J1 is J - 1,
     nCartas_i(N,N,J,L),
     nCartas(N,J1,Xs).
+
+n2Cartas_k(N,I,J,K,L):-
+    K = 0,L = [1].
+n2Cartas_k(N,I,J,K,[Y | Xs]) :-
+    K > 0,
+    Y is (N+2+N*(K-1)+((I-1)*(K-1)+J-1)),
+    K1 is K - 1,
+    n2Cartas_k(N,I,J,K1,Xs).
+
+n2Cartas_j(N,I.J,L):-
+    J = 0, L = [1].
+n2Cartas_j(N,I,J,L) :-
+    J>0,
+    J1 is J - 1 ,
+    n2Cartas_k(N,N,J,N,Lout),
+    n2Cartas_j(N,I,J1,[Lin|Lout]).
+
+
+n2Cartas(N,I,L):-
+    I = 0, L = [].
+n2Cartas(N,I,L) :-
+    I>0,
+    I1 is I - 1,
+    n2Cartas_j(N,N,N,L),
+    n2Cartas(N,I1,Xs).
